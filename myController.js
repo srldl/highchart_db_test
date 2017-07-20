0bad29ec06bfc35d7d52adbf2fb0b2b59a89bae9
srldl <siri.uldal@gmail.com>
2017-07-20 14:44:09 +0200

    listing pie- and barchart ok

--- myController.js
+++ myController.js
@@ -8,6 +8,7 @@
                     console.log(limit_dates);
 
                     var id = "a9fb0ae5ad56720b48766d8db219217e";
+                    //var id = "d9f287b7b7fd667d175b5a60280023e7";
 
                     //Set link for now - demo purposes
                     var link = "http://api-test.data.npolar.no/statistic/" + id;
@@ -15,16 +16,30 @@
                     //Fetch search result
                     dbSearch.getValues(link).then(
                           function(results) {
-                              // on success
-                              console.log(results.data);
-                              $scope.all = results.data;
+                               // on success
+                               console.log(results.data);
+                               $scope.all = results.data;
 
-                               console.log(limit_dates);
+                               //Create piechart array
+                               var piechart = new Array(($scope.all.component).length);
+                               for (var j = 0; j < piechart.length; j++) {
+                                    piechart[j] = new Array(($scope.all.component[j].visuals).length);
+                               }
 
+                               //Create barchart array
+                               var barchart = new Array(($scope.all.component).length);
+                               for (var i = 0; i < barchart.length; i++) {
+                                    barchart[i] = new Array(($scope.all.component[i].visuals).length);
+                               }
 
-                                        $scope.barchart =
+                                  $scope.barchart = barchart;
+                                  $scope.piechart = piechart;
+                                  console.log($scope.barchart);
+                                  console.log($scope.piechart);
+
+                                    $scope.barchart[0][1] =
                                       [{
-                                        name: 'researchzzz',
+                                        name: 'research[0][0]',
                                         y: 56.33
                                     }, {
                                         name: 'topographical mapping',
@@ -40,18 +55,60 @@
                                         y: 0.91
                                     }];
 
-                                     // Sample data for pie chart
-                                        $scope.piechart = [{
-                                                name: "Fieldwork",
+                                    $scope.barchart[0][2] =
+                                      [{
+                                        name: 'research[1][0]',
+                                        y: 56.33
+                                    }, {
+                                        name: 'topographical mapping',
+                                        y: 24.03
+                                    }, {
+                                        name: 'outreach VIP',
+                                        y: 10.38
+                                    }, {
+                                        name: 'logistic operations',
+                                        y: 4.77
+                                    }, {
+                                        name: 'other',
+                                        y: 0.91
+                                    }];
+
+
+                                    $scope.barchart[0][4] =
+                                      [{
+                                        name: 'research[1][1]',
+                                        y: 56.33
+                                    }, {
+                                        name: 'topographical mapping',
+                                        y: 24.03
+                                    }, {
+                                        name: 'other',
+                                        y: 0.91
+                                    }];
+
+                                     //Sample data for pie chart
+                                     $scope.piechart[0][0] = [{
+                                                name: "Fieldwork0",
                                                 y: 56.33
                                             }, {
-                                                name: "Cruise",
+                                                name: "Cruise0",
                                                 y: 24.03,
                                                 sliced: true,
                                                 selected: true
-                                        }]
+                                     }]
 
+                                     // Sample data for pie chart
+                                     $scope.piechart[0][3] = [{
+                                                name: "Fieldwork3",
+                                                y: 56.33
+                                            }, {
+                                                name: "Cruise3",
+                                                y: 24.03,
+                                                sliced: true,
+                                                selected: true
+                                     }]
 
+                                    console.log($scope.barchart);
 
 
                     }); //end getValues
